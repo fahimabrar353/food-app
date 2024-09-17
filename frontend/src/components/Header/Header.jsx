c1e523391906b195cd822f9acf855d41c4b1df52
shahreen <140973098+ShahreenHRabbani@users.noreply.github.com>
2024-09-17 13:44:42 +0600

    feat: few changes

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
-import './Header.css'
+
+import React, { useState, useEffect } from 'react';
+import './Header.css';
+import menuImage from '../../assets/header/header_image_02.jpg';
+
+
 
 const Header = () => {
+  const words = ['Order', 'Have', 'Enjoy'];
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentWordIndex((prevIndex) => (prevIndex + 1) % words.length);
+    }, 1000); // Change word every 1 second
+
+    return () => clearInterval(interval); // Cleanup interval on component unmount
+  }, [words.length]);
     return (
         <div className='header'>
-            <div className='header-contents'>
-                <h2>Order your favourite food here</h2>
-                <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
-                <button>View Menu</button>
-            </div>
+           <div className='header-contents'>
+            <h1>{words[currentWordIndex]}</h1>
+            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+           </div>
+           <div className='header-images'>
+             <img className='img' src={menuImage} alt="Image in Box 2"/>
+           </div>
         </div>
     )
 }
 
 export default Header
+
+
